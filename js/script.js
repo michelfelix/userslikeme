98b5e9aa24cb64e3f955ef8d570ab182e6f55038
Michel Felix <micheldias9@gmail.com>
2021-07-02 00:00:29 -0300

    Consumindo a API e trazendo as informações de maneira dinâmica

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ function joinWords(a, b){
     return sealed
 }
 
-
 function joinPlaces(a, b){
     var sealed = a + ', ' + b
     return sealed
@@ -16,11 +15,13 @@ fetch('https://randomuser.me/api/')
 .then(function(data) {
   let user = data.results;
   console.log(user)
+  var backgroundImage = document.getElementById("photoBackground");
   return user.map(function(user) {
       var userFirstName = user.name.first;
       var userLastName = user.name.last;
       var userLocationCity = user.location.city;
       var userLocationCountry = user.location.country;
+      var userPhotoPicture = user.picture.large;
     document.getElementById("userName").innerHTML = joinWords(
       userFirstName,
       userLastName
@@ -30,6 +31,11 @@ fetch('https://randomuser.me/api/')
       userLocationCity,
       userLocationCountry
     );
+
+    document.getElementById("photoPicture").innerHTML =
+    `<img src="${userPhotoPicture}">`;
+
+    backgroundImage.style.backgroundImage = `URL(${userPhotoPicture})`;
   })
 })
 }
