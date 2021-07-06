074f25061efb275250df6e744fc66c9c8a508a49
Michel Felix <micheldias9@gmail.com>
2021-07-06 18:47:56 -0300

    Os usuários que são seguidos tem suas informações salvas no localstorage

--- userslikeme/src/app/js/script.js
+++ userslikeme/src/app/js/script.js
@@ -1,44 +1,57 @@
-function joinWords(a, b){
-    var sealed = a + ' ' + b
-    return sealed
+var currentUser
+
+function getInfoUser() {
+  fetch("https://randomuser.me/api/")
+    .then((resp) => resp.json())
+    .then(function (data) {
+      let user = data.results;
+      currentUser = user;
+      var backgroundImage = document.getElementById("photoBackground");
+      return user.map(function (user) {
+        var userFirstName = user.name.first;
+        var userLastName = user.name.last;
+        var userLocationCity = user.location.city;
+        var userLocationCountry = user.location.country;
+        var userPhotoPicture = user.picture.large;
+
+        document.getElementById("userName").innerHTML = [
+          userFirstName,
+          userLastName,
+        ].join(" ");
+
+        document.getElementById("country").innerHTML = [
+          userLocationCity,
+          userLocationCountry,
+        ].join(", ");
+
+        document.getElementById(
+          "photoPicture"
+        ).innerHTML = `<img class="user-photo" src="${userPhotoPicture}">`;
+
+        backgroundImage.style.backgroundImage = `URL(${userPhotoPicture})`;
+      });
+    });
 }
 
-function joinPlaces(a, b){
-    var sealed = a + ', ' + b
-    return sealed
+function toggleFollowButton() {
+  const button = document.getElementById("follow");
+
+  button.classList.toggle("btn-danger");
+  if (button.innerHTML === "Follow") {
+    button.innerHTML = "Unfollow";
+  } else {
+    button.innerHTML = "Follow";
+  }
 }
 
-function infoUser() {
-
-fetch('https://randomuser.me/api/')
-.then((resp) => resp.json())
-.then(function(data) {
-  let user = data.results;
-  console.log(user)
-  var backgroundImage = document.getElementById("photoBackground");
-  return user.map(function(user) {
-      console.log(user.id.value)
-      var userFirstName = user.name.first;
-      var userLastName = user.name.last;
-      var userLocationCity = user.location.city;
-      var userLocationCountry = user.location.country;
-      var userPhotoPicture = user.picture.large;
-    document.getElementById("userName").innerHTML = joinWords(
-      userFirstName,
-      userLastName
-    );
-
-    document.getElementById("country").innerHTML = joinPlaces(
-      userLocationCity,
-      userLocationCountry
-    );
-
-    document.getElementById("photoPicture").innerHTML =
-    `<img class="user-photo" src="${userPhotoPicture}">`;
-
-    backgroundImage.style.backgroundImage = `URL(${userPhotoPicture})`;
-  })
-})
+function store(key, data) {
+  if (localStorage.getItem(key) === null) {
+        localStorage.setItem(key, JSON.stringify([data]));
+  } else {
+    localStorage.setItem(key, JSON.stringify([ ...JSON.parse(localStorage.getItem(key)), data ]))
+  }
 }
 
-infoUser()
+function followUser() {
+  store("users", currentUser)
+}
