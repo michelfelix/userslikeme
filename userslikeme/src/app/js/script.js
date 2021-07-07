e234e9067a023989f0d513e526c88a70c90f3673
Michel Felix <micheldias9@gmail.com>
2021-07-07 00:41:54 -0300

    Foi feito a inserção das informações dos usuários que vieram pela API

--- userslikeme/src/app/js/script.js
+++ userslikeme/src/app/js/script.js
@@ -13,6 +13,10 @@ function getInfoUser() {
         var userLocationCity = user.location.city;
         var userLocationCountry = user.location.country;
         var userPhotoPicture = user.picture.large;
+        var userBornAt = user.location.country;
+        var userAge = user.dob.age;
+        var userEmail = user.email;
+        var userCellPhone = user.cell;
 
         document.getElementById("userName").innerHTML = [
           userFirstName,
@@ -29,6 +33,14 @@ function getInfoUser() {
         ).innerHTML = `<img class="user-photo" src="${userPhotoPicture}">`;
 
         backgroundImage.style.backgroundImage = `URL(${userPhotoPicture})`;
+
+        document.getElementById("bornAt").innerHTML = `born at: ${userBornAt}`;
+
+        document.getElementById("age").innerHTML = `Age: ${userAge} years old`;
+
+        document.getElementById("email").innerHTML = `email: ${userEmail}`;
+
+        document.getElementById("phone").innerHTML = `Phone1: ${userCellPhone}`;
       });
     });
 }
